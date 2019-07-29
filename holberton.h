4f03818c96931e30587d17ef22d68d38ab248d33
zelda096 <966@holbertonschool.com>
2019-07-29 20:49:14 +0000

    update holberton.h

--- holberton.h
+++ holberton.h
@@ -4,13 +4,19 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-int _putchar(char c);
+/**
+ * struct type - struct for all printf / protoypes
+ * @type: s, c, i, d, etc.
+ * @f: pointer to function
+ */
 
 typedef struct type
 {
-  char type;
-  int (*f)(va_list args);
+	char type;
+	int (*f)(va_list args);
 } type_t;
+
+int _putchar(char c);
 
 int (*getTypes(char type))(va_list args);
 
